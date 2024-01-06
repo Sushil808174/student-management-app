9536cfc17ddf0eebb35693f8948f7faa3bc5a698
Susheel Kumar <sushilkumar8081742575@gmail.com>
2024-01-06 16:51:02 +0530

    added controller written all logic

--- School-Management/src/main/java/com/schoolManagement/repository/StudentRepository.java
+++ School-Management/src/main/java/com/schoolManagement/repository/StudentRepository.java
@@ -3,5 +3,10 @@ package com.schoolManagement.repository;
 import com.schoolManagement.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student,Integer> {
+    public List<Student> findByName(String name);
+
+    public List<Student> findByCourses_Id(Integer courseId);
 }
