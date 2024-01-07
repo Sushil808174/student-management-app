5ad95e95319c5159fb74ca7e2a2de4bca348599b
Susheel Kumar <115461689+Sushil808174@users.noreply.github.com>
2024-01-07 21:48:04 +0530

    Update StudentController.java

--- Student-Management/src/main/java/com/studentManagement/controller/StudentController.java
+++ Student-Management/src/main/java/com/studentManagement/controller/StudentController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/STUDENT")
 public class StudentController {
     @Autowired
     private StudentService studentService;
