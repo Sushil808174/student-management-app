174eaf4c4cffbe85335aeb4dc5bf7cc24794fc47
Susheel Kumar <115461689+Sushil808174@users.noreply.github.com>
2024-01-07 15:40:33 +0530

    Delete School-Management directory

--- School-Management/src/main/java/com/schoolManagement/repository/AdminRepository.java
+++
@@ -1,7 +0,0 @@
-package com.schoolManagement.repository;
-
-import com.schoolManagement.entity.Admin;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AdminRepository extends JpaRepository<Admin,Integer> {
-}
--- School-Management/src/main/java/com/schoolManagement/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-package com.schoolManagement.repository;
-
-import com.schoolManagement.entity.Course;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseRepository extends JpaRepository<Course,Integer> {
-}
